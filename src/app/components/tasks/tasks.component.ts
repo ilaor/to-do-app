ee5b5eb7c9734c38917fedc89955911b7bbf0260
ivan <ivanlaraorta@gmail.com>
2026-04-20 10:36:25 +0200

    Form gestión de tareas

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,9 +1,67 @@
 import { Component } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-tasks',
   standalone: false,
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css',
+  styleUrls: ['./tasks.component.css'],
+  providers: [TaskService]
 })
-export class TasksComponent {}
+export class TasksComponent {
+  tasks: Task[] = [];
+  titulo: string = '';
+
+  constructor(private taskService: TaskService) {
+    this.taskService.getTasks()
+      .subscribe(tasks => {
+        this.tasks = tasks;
+        console.log({ tasks: this.tasks });
+      });
+  }
+
+  addTask(event: any) {
+    event.preventDefault(); // no queremos que refresque la pantalla
+    const newTask: Task = {
+      title: this.titulo,
+      isDone: false
+    };
+
+    this.taskService.addTask(newTask)
+      .subscribe(task => {
+        this.tasks.push(task);
+        this.titulo = '';
+      });
+  }
+
+  deleteTask(id: any) {
+    const response = confirm('¿Estás seguro de que deseas elimiarlo?');
+    if (response) {
+      const tasks = this.tasks;
+      this.taskService.deleteTask(id)
+        .subscribe(data => {
+          console.log(data.n);
+          if (data.n == 1) {
+            for (let i = 0; i < tasks.length; i++) {
+              if (tasks[i]._id == id) {
+                tasks.splice(i, 1);
+              }
+            }
+          }
+        });
+    }
+  }
+
+  updateStatus(task: Task) {
+    const newTask = {
+      title: task.title,
+      isDone: !task.isDone
+    };
+
+    this.taskService.updateTask(task._id, newTask)
+      .subscribe(res => {
+        task.isDone = !task.isDone;
+      });
+  }
+}
