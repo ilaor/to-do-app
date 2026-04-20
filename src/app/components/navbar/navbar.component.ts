6dd46c1ab96b58ff784974cec7efffa89e7ef2ee
ivan <ivanlaraorta@gmail.com>
2026-04-20 10:27:19 +0200

    NavBar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { UserService } from "../../services/user.service";
 
 @Component({
   selector: 'app-navbar',
@@ -6,4 +8,27 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {}
+export class NavbarComponent implements OnInit {
+  title = 'toDoApp para SD';
+  displayName = '';
+
+  constructor(
+    public userService: UserService, 
+    public router: Router
+  ) { }
+
+  ngOnInit(): void {
+    console.log('ngOnInit: navbar');
+    this.userService.getUser().subscribe({
+      next: user => {
+        console.log("Usuario cargado: ", user);
+        this.displayName = user.displayName;
+      },
+      error: err => {
+        console.error("Error cargando datos del usuario");
+        console.log(err);
+        this.router.navigateByUrl("/login");
+      }
+    });
+  }
+}
